bbd3413049dea7b6ec8537bd431660a6b69f6fbd
Yaniv Zimet <yaniv@codeforamerica.org>
2015-05-07 09:44:30 -0700

    added accounts, server-side calls

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,18 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
 	Template.body.helpers({
 		tasks: function() {
-			return Tasks.find({}, {sort: {createdAt: -1}});
+			if (Session.get("hideCompleted")) {
+				// filter tasks
+				return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+			} else {
+				return Tasks.find({}, {sort: {createdAt: -1}});
+			}
+		},
+		hideCompleted: function() {
+			return Session.get("hideCompleted");
+		},
+		incompleteCount: function() {
+			return Tasks.find({checked: {$ne: true}}).count();
 		}
 	});
 	
@@ -12,16 +23,16 @@ if (Meteor.isClient) {
 		"submit .new-task": function(event) {
 			var text = event.target.text.value;
 			
-			Tasks.insert({
-				text: text,
-				createdAt: new Date()
-			});
+			Meteor.call("addTask", text);
 			
 			// clear form
 			event.target.text.value = "";
 			
 			// prevent deafult form submit
 			return false;
+		},
+		"change .hide-completed input": function(event) {
+			Session.set("hideCompleted", event.target.checked);
 		}
 	});
 	
@@ -34,6 +45,11 @@ if (Meteor.isClient) {
 			Tasks.remove(this._id);
 		}
 	});
+	
+	// confituge the accounds UI to use usernames instead of email
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_ONLY"
+	});
 }
 
 if (Meteor.isServer) {
@@ -41,3 +57,17 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Meteor.methods({
+	addTask: function(text) {
+		if (!Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+		Tasks.insert({
+			text: text,
+			createdAt: new Date(),
+			owner: Meteor.userId(), // id of logged in user
+			username: Meteor.user().username // username of logged in user
+		});
+	}
+});
